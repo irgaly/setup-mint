ec0a0bd7caf4b6a5e838eda380f91a9f2f67b696
irgaly <irgaly@gmail.com>
2022-02-18 18:55:15 +0900

    add ts-node

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,29 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import { ExecOptions } from '@actions/exec'
+
+const execute = async (command: string): Promise<string> => {
+  let output = ''
+  const options: ExecOptions = {}
+  options.listeners = {
+    stdout: (data: Buffer) => {
+      output += data.toString()
+    },
+    stderr: (data: Buffer) => {
+      console.error(data)
+    }
+  }
+  await exec.exec(command, null, options)
+  return output
+}
 
 async function run() {
-  core.debug('test')
+  try {
+    const useCache = core.getInput('use-cache')
+    core.debug("useCache: ${useCache}")
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
 
 run()
