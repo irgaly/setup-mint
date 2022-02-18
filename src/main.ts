da99b3388c758b0c54087c120d44a96cf175cca2
irgaly <irgaly@gmail.com>
2022-02-18 19:54:19 +0900

    read Mintfile

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import { ExecOptions } from '@actions/exec'
+import * as fs from 'fs'
 
 async function execute(command: string): Promise<string> {
   let output = ''
@@ -19,8 +20,19 @@ async function execute(command: string): Promise<string> {
 
 async function main() {
   try {
+    const bootstrap = (core.getInput('bootstrap') == 'true')
     const useCache = (core.getInput('use-cache') == 'true')
+    core.debug(`bootstrap: ${bootstrap}`)
     core.debug(`useCache: ${useCache}`)
+    let mintVersion = '0.17.0'
+    if (fs.existsSync('Mintfile')) {
+      const mintfile = fs.readFileSync('Mintfile').toString()
+      const match = mintfile.match(/^\s*yonaskolb\/mint@([^\s#]+)/)
+      if (match) {
+        mintVersion = match[1]
+        core.debug(`mintVersion from Mintfile: ${mintVersion}`)
+      }
+    }
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
